e914b37acc4a8dc3fa106d61166d64a8bd05e95c
wernermendezz <50336778+wernermendezz@users.noreply.github.com>
2019-06-01 13:17:55 -0600

    Add files via upload
    probando git

---
+++ MP3/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace MP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.reproductor = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.reproductor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Con Windows Media";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // reproductor
+            // 
+            this.reproductor.Enabled = true;
+            this.reproductor.Location = new System.Drawing.Point(44, 121);
+            this.reproductor.Name = "reproductor";
+            this.reproductor.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("reproductor.OcxState")));
+            this.reproductor.Size = new System.Drawing.Size(432, 202);
+            this.reproductor.TabIndex = 2;
+            this.reproductor.Enter += new System.EventHandler(this.reproductor_Enter);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(551, 379);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Play";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(546, 325);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 27);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Con MCI";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(653, 378);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 474);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.reproductor);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.reproductor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private AxWMPLib.AxWindowsMediaPlayer reproductor;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ MP3/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace MP3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //Al cargar el formulario ponemos el reproductor como invisible
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            reproductor.uiMode = "invisible";
+        }
+        
+        //Reproducir A través de Windows Player
+        private void button1_Click(object sender, EventArgs e)
+        {           
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                reproductor.URL = openFileDialog1.FileName;                 
+            }
+            reproductor.Ctlcontrols.play();
+        }
+      
+
+        //Detener la reproducción en Windows Player
+        private void button2_Click(object sender, EventArgs e)
+        {
+            reproductor.Ctlcontrols.stop();
+        }
+
+
+        //DLL a utilizar para poder reproducir MP3
+       [DllImport("winmm.dll")]
+
+        //Método externo (esta definido en winmm.dll) tipo long que se encargara de enviar comandos al MCI
+        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+
+
+        //Método para reproducir MP3 a través de MCI
+        //Recibe: Nombre y ruta del archivo a reproducir
+        public void PlayMP3(string rutaArchivo)
+        {
+            //Comandos multimedia de MCI http://msdn.microsoft.com/en-us/library/ms712587        
+
+            //Abrir el dispositivo MCI
+            //miMP3 es el alias con el que manejaremos el archivo MP3 recibido como parametro en rutaArchivo
+            string comandoMCI = string.Format("open \"{0}\" type mpegvideo alias miMP3", rutaArchivo);
+            //a traves de mciSendString, enviamos el comando anterior, para abrir el dispositivo MCO
+            mciSendString(comandoMCI, null, 0, IntPtr.Zero);
+            //Ahora en comandoMCI daremos la orden de reproducir el archivo, recordando que lo hacemos
+            //a traves del alias que definimos anteriormente miMP3
+            comandoMCI = "play miMP3";
+            //enviamos a ejecutar el comando play
+            mciSendString(comandoMCI, null, 0, IntPtr.Zero);
+        }
+
+       //Reproducir a través de MCI, 
+        //Envia: el nombre del archivo se envia hacia el método PlayMP3
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                PlayMP3(openFileDialog1.FileName); 
+            }
+            
+
+        }
+
+        //Detener la reproducción del MP3 en el MCI
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Comandos multimedia de MCI http://msdn.microsoft.com/en-us/library/ms712587            
+            //el comando es stop y se le envia al alias miMP3, que se definio cuando se dio Play
+            string comandoMCI = "stop miMP3";
+            //Enviar el comando stop al MCI
+            mciSendString(comandoMCI, null, 0, IntPtr.Zero);
+        }
+
+        private void reproductor_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
